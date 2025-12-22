d6d59eaa88cd38de6898b86817ead6029ccb9cd7
criscat8 <77325933+criscat8@users.noreply.github.com>
2025-12-22 14:02:08 +0800

    Configure custom domain for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,9 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-const isProd = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
-  site: isProd ? 'https://chva-solutions.github.io' : 'http://localhost:4321',
-  base: isProd ? '/tinvail-page/' : '/',
+  site: 'https://tinvail.art',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
